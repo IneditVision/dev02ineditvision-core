e731d12fd7678dc84849b9de48fe539983b75b52
root <root@server6.ineditvision.com>
2022-03-11 13:11:32 +0200

    intermediar

--- db/Database.php
+++ db/Database.php
@@ -11,7 +11,7 @@ use Ineditvision\Dev02Ineditvision\core\Application;
  *
  */
 class Database {
-    public $pdo;                            //public \PDO $pdo;
+    public $pdo;                            //public \PDO $pdo;     //pt PHP >=7.4
 
     public function __construct($config) {
         $dsn = $config['dsn'] ?? '';
